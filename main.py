829336dc699ddf5c6c2f214a7f266f11788bdda5
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2023-08-02 17:48:08 -0400

    chore: remove white background from watermark images

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def read_folder_contents(folder_path):
     contents = []
     for path in os.listdir(folder_path):
         full_path = os.path.join(folder_path, path)
+        if path.startswith("."):
+            continue
+
         if os.path.isfile(full_path):
             contents.append(full_path)
     
@@ -27,9 +30,14 @@ def resize(watermark):
 
     return resized
 
-def append_watermark_to_image(name, image, watermark_image):
-    resized_watermark = resize(watermark_image)
+def show(img):
+    cv2.imshow("Invert1",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+def append_watermark_to_image(name, image, watermark_image):
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+    resized_watermark = resize(watermark_image)
     height, width, _ = image.shape
     center_y, center_x = image_center(image)
 
@@ -42,7 +50,7 @@ def append_watermark_to_image(name, image, watermark_image):
     right_x = left_x + watermark_width
     
     destination = image[top_y:bottom_y, left_x:right_x]
-    result = cv2.addWeighted(destination, 1, resized_watermark, 0.2, 0)
+    result = cv2.addWeighted(destination, 1, resized_watermark, 0.1, 0)
     image[top_y:bottom_y, left_x:right_x] = result
 
     cv2.imwrite(name + "watermarked.jpg", image)
@@ -51,7 +59,7 @@ def append_watermark_to_image(name, image, watermark_image):
 def run_watermark(folder_path, watermark_path):
     contents = read_folder_contents(folder_path)
     watermark_image = cv2.imread(watermark_path)
-
+    watermark_image = cv2.bitwise_not(watermark_image)
     for image_path in contents:
         loaded_image = cv2.imread(image_path)
         image_name = os.path.splitext(image_path)[0]
